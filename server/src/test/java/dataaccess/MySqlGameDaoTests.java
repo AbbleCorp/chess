182badccfff5bb8682504b3486b1f4bf71ec6b0d
Abbie Omer <aomer942@byu.edu>
2024-10-31 12:45:52 -0600

    Wrote some GameDAO tests

--- server/src/test/java/dataaccess/MySqlGameDaoTests.java
+++ server/src/test/java/dataaccess/MySqlGameDaoTests.java
@@ -2,30 +2,51 @@ package dataaccess;
 
 import chess.ChessGame;
 import model.GameData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class MySqlGameDaoTests {
+    DatabaseManager DB;
     private GameDAO gameDB;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws DataAccessException {
+        DB = new DatabaseManager();
+        DB.configureDatabase();
         gameDB = new MySqlGameDAO();
-    }
-
-    @Test
-    void clear() {
         gameDB.clear();
     }
 
     @Test
+    void testClear() throws DataAccessException {
+        gameDB.createGame("white","black","game1",new ChessGame());
+        gameDB.createGame(null,null,"game2",new ChessGame());
+        gameDB.clear();
+        ArrayList<GameData> gameList = gameDB.listGames();
+        Assertions.assertTrue(gameList.isEmpty());
+    }
+
+    //positive
+    @Test
     void createGame() throws DataAccessException {
-        int game1 = gameDB.createGame("white1",null,"game1", new ChessGame());
-        System.out.print(game1);
-        int game2 = gameDB.createGame(null,null,"game2",new ChessGame());
-        System.out.print(game2);
+        gameDB.createGame("white1",null,"game1", new ChessGame());
+        gameDB.createGame(null,null,"game2",new ChessGame());
+        ArrayList<GameData> gameList = gameDB.listGames();
+        Assertions.assertEquals(2,gameList.size());
+        Assertions.assertEquals("game1",gameList.get(0).gameName());
+    }
+
+
+    //negative
+    @Test
+    void createGameNullGameName() throws DataAccessException {
+        Exception e = Assertions.assertThrows(DataAccessException.class, () -> {
+            gameDB.createGame(null,null,null,new ChessGame());
+        });
+        Assertions.assertEquals("Column 'gameName' cannot be null",e.getMessage());
     }
 
     @Test
