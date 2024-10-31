9f8baa4835ddd72440c098b4ec45ae9319b7730f
Abbie Omer <aomer942@byu.edu>
2024-10-30 18:50:49 -0600

    Fixed createAuth to insert new auth or update if user already exists

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -47,10 +47,11 @@ public class MySqlAuthDAO implements AuthDAO {
     @Override
     public AuthData createAuth(String username) {
         try (var conn = DatabaseManager.getConnection()) {
-            try (var preparedStatement = conn.prepareStatement("INSERT INTO auth (username, authToken) VALUES(?, ?)")) {
+            try (var preparedStatement = conn.prepareStatement("INSERT INTO auth (username, authToken) VALUES(?, ?)" + "ON DUPLICATE KEY UPDATE authToken=?")) {
                 String token = UUID.randomUUID().toString();
                 preparedStatement.setString(1, username);
                 preparedStatement.setString(2, token);
+                preparedStatement.setString(3,token);
                 preparedStatement.executeUpdate();
                 return new AuthData(username, token);
             }
